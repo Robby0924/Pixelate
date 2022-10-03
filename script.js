66f5d0b13352a7e43a3e5e10adb4b5416c2681b4
Antoine Coates <antoinecoates236@gmail.com>
2022-10-03 13:17:09 -0600

    secondswitch

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 // Your code here
 
-let table = document.getElementsByTagName('table')
+let table = document.getElementsByTagName('table') [0]
 
 function makeRow () {
     const tableRow = document.createElement ('tr')
     for (let i = 0; i<20; i++){
-    const tableData = document.parentElement('td').appendChild(td)
+    const tableData = document.createElement('td')
+    tableRow.appendChild(tableData)
     }
+    table.appendChild(tableRow)
+}
+
+const button = document.getElementById('add-row')
+button.addEventListener('click', makeRow)
+
+table.addEventListener('click', colorize)
+let select = document.getElementById('drop-down')
+
+color = ''
+select.addEventListener('change', function(event){
+color = event.target.value
+
+})
+
+function colorize(event){
+const pacman = event.target
+if (pacman.className.length){
+    pacman.className = ''
+} else{
+     pacman.className = color
+ }
+
+ 
+
 }
 
 
-makeRow()
-makeRow()
-makeRow()
-makeRow()
-makeRow()
-makeRow()
-makeRow()
-makeRow()
-makeRow()
+
+
+
+
