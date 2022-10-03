7df3e153dcc5bd1d6771aeacc63b057f74800246
Robby Bacus <robbybacus@gmail.com>
2022-10-03 10:55:35 -0400

    1st switch

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your code here
+
+let table = document.getElementsByTagName('table')
+
+function makeRow () {
+    const tableRow = document.createElement ('tr')
+    for (let i = 0; i<20; i++){
+    const tableData = document.parentElement('td').appendChild(td)
+    }
+}
+
+
+makeRow()
+makeRow()
+makeRow()
+makeRow()
+makeRow()
+makeRow()
+makeRow()
+makeRow()
+makeRow()
